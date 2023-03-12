20a8877af7612b97eb3e6d3248593392653bbd22
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2023-03-12 00:48:53 -0300

    Added dynamic open-api specs

--- KsClothing/urls.py
+++ KsClothing/urls.py
@@ -2,11 +2,31 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("rest_framework.urls")),
     path("store/", include("store.urls")),
+    path(
+        "openapi",
+        get_schema_view(
+            title="Clothing Store API",
+            description="API for monitoring stock and registering sales.",
+            version="1.0.0",
+        ),
+        name="openapi-schema",
+    ),
+    path(
+        "swagger-ui/",
+        TemplateView.as_view(
+            template_name="swagger-ui.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+        name="swagger-ui",
+    ),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
