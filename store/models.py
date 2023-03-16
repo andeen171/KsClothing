9f4928a5a28f3557de7024f6ea20c18fd2fe5fb2
Anderson Ribeiro Lopes <73368853+andeen171@users.noreply.github.com>
2023-03-16 17:26:18 -0300

    optional image field for product model

--- store/models.py
+++ store/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/", blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
